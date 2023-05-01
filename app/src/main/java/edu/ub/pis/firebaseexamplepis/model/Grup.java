859b5c29e12c736dc317ae917a89c3647e1c0425
Tomas Aladjem <samot2003@gmail.com>
2023-05-01 16:55:59 +0200

    He creat els dissenys de les pestanyes de grups

---
+++ app/src/main/java/edu/ub/pis/firebaseexamplepis/model/Grup.java
@@ -0,0 +1,55 @@
+package edu.ub.pis.firebaseexamplepis.model;
+
+import java.util.ArrayList;
+
+public class Grup {
+    private ArrayList<Message> messages;
+    private String missatge;
+    private ArrayList<User> usuaris;
+
+    public Grup(String idUser1, String idUser2, String missatge) {
+        UserRepository uRepo = UserRepository.getInstance();
+        this.messages = new ArrayList<>();
+        this.missatge = missatge;
+    }
+
+    public void addMessage(Message message) {
+        messages.add(message);
+    }
+
+    public ArrayList<Message> getMessages() {
+        return messages;
+    }
+
+    public User getUser1() {
+        return user1;
+    }
+
+    public User getUser2() {
+        return user2;
+    }
+
+    public void setUser1(User user1) {
+        this.user1 = user1;
+    }
+
+    public void setUser2(User user2) {
+        this.user2 = user2;
+    }
+
+    public User getUser(User currentUser) {
+        if (user1 == currentUser){
+            return user2;
+        }else if (user2 == currentUser){
+            return user1;
+        }else{
+            throw new RuntimeException("user not in chat");
+        }
+    }
+
+    public String getMessage() {
+        return missatge;
+    }
+}
+
+
